1ffa46d950514364998fce7753c1c14ac579357f
Benefice0922 <mikhaselkin@icloud.com>
2025-10-07 04:11:29 +0300

    q

--- scam-bot/handlers/functions/getCurCode.js
+++
@@ -1,9 +0,0 @@
-module.exports = async function getCode(currency) {
-  const codes = {
-    PLN: "zł",
-    EUR: "€",
-    DKK: "kr",
-  };
-
-  return codes[currency];
-};
--- scam-bot/launch.js
+++
@@ -1,2 +0,0 @@
-const bot = require("./bot")
-const web = require("./backend/web")
