a4e684c5f8c4c035c72854d737cd4c090920e1a7
Benefice0922 <mikhaselkin@icloud.com>
2025-10-07 04:44:16 +0300

    й

--- sender/config.py
+++
@@ -1,6 +0,0 @@
-BOT_TOKEN = "8226987147:AAFeXz0O5xZWSG4wk7eZ31LzcSm9BEkP1Vw"
-ADMIN_IDS = [8478194849,8401470687]  # Твой ID и, при желании, других админов
-ALLOWED_USER_IDS = [
-    8478194849,  # ваш Telegram user_id
-    8401470687,  # user_id второго пользователя и т.д.
-]
--- sender/handlers/accounts_fsm.py
+++
@@ -1,7 +0,0 @@
-from aiogram.fsm.state import State, StatesGroup
-
-class AccountsStates(StatesGroup):
-    waiting_for_accounts = State()
-
-class CookieStates(StatesGroup):
-    waiting_for_cookie_file = State()
